d116ecf586d187d91b02c4836a48407c6cb82b7d
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-06-05 03:13:56 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Deep Learning for chemical structures',
+    author='Alex Federation',
+    license='MIT',
+)
